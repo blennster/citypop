b9caf3350a72d67d095404c95da18179e7430d04
Emil Blennow <emil.blennow99@gmail.com>
2020-11-18 18:18:48 +0100

    Added documentation

--- src/util.ts
+++ src/util.ts
@@ -4,9 +4,16 @@ const API_URL = 'http://api.geonames.org/searchJSON';
 const USERNAME = 'weknowit';
 const ROW_COUNT = 10;
 
+// Check out http://www.geonames.org/export/codes.html for more info.
 const COUNTRY_CLASS = 'A';
 const CITY_CLASS = 'P';
 
+/**
+ * Perform a search on api endpoint. Should be used for general
+ * searches.
+ * @param q - The query string.
+ * @param isCountry - If you are looking for country or city.
+ */
 const apiSearchQuery = async (
   q: string,
   isCountry: boolean,
@@ -28,7 +35,14 @@ const apiSearchQuery = async (
   }
 };
 
-const apiQuery = async (country: string, city: string): Promise<GeoNames.RootObject | null> => {
+/**
+ * Perform a specify search query.
+ * NOTE: A search without the city supplied will return
+ * all the cities within that country ordered by population.
+ * @param country - The country that is being searched after
+ * @param city - The city that is being searched after
+ */
+const apiQuery = async (country: string, city = ''): Promise<GeoNames.RootObject | null> => {
   try {
     const res = await axios.get<GeoNames.RootObject>(API_URL, {
       params: {
