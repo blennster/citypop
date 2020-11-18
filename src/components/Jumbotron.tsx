b7cbfbba83c0eeb972483d398c9fe7e1cb72af87
Emil Blennow <emil.blennow99@gmail.com>
2020-11-18 16:22:07 +0100

    BEM compliance

--- src/components/Jumbotron.tsx
+++ src/components/Jumbotron.tsx
@@ -7,7 +7,7 @@ type JumbotronProps = {
 };
 
 const Jumbotron: React.FunctionComponent<JumbotronProps> = ({ title = 'CityPop', subHeader }) => (
-  <div className="wrapper">
+  <div className="jumbotron__wrapper">
     <div>
       <h1>{ title }</h1>
       { subHeader != null ? <h3>{ subHeader }</h3> : null }
