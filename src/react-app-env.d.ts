39c7f30430016800961380ed61eded4f7d28bd88
Emil Blennow <emil.blennow99@gmail.com>
2020-11-18 10:12:46 +0100

    Added interface to api type

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,1 +1,31 @@
 /// <reference types="react-scripts" />
+declare namespace GeoNames {
+
+  export interface AdminCodes1 {
+    ISO3166_2: string;
+  }
+
+  export interface Geoname {
+    adminCode1: string;
+    lng: string;
+    geonameId: number;
+    toponymName: string;
+    countryId: string;
+    fcl: string;
+    population: number;
+    countryCode: string;
+    name: string;
+    fclName: string;
+    countryName: string;
+    fcodeName: string;
+    adminName1: string;
+    lat: string;
+    fcode: string;
+    adminCodes1: AdminCodes1;
+  }
+
+  export interface RootObject {
+    totalResultsCount: number;
+    geonames: Geoname[];
+  }
+}
