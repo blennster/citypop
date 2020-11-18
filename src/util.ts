bbc7b2427ea6e2062922348bbd5d67baa58ee59e
Emil Blennow <emil.blennow99@gmail.com>
2020-11-18 18:12:00 +0100

    Added constants

--- src/util.ts
+++ src/util.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 const API_URL = 'http://api.geonames.org/searchJSON';
+const USERNAME = 'weknowit';
+const ROW_COUNT = 10;
+
+const COUNTRY_CLASS = 'A';
+const CITY_CLASS = 'P';
 
 const apiSearchQuery = async (
   q: string,
@@ -10,10 +15,10 @@ const apiSearchQuery = async (
     const res = await axios.get<GeoNames.RootObject>(API_URL, {
       params: {
         q,
-        maxRows: 10,
-        username: 'weknowit',
+        maxRows: ROW_COUNT,
+        username: USERNAME,
         orderBy: 'relevance',
-        featureClass: isCountry ? 'A' : 'P',
+        featureClass: isCountry ? COUNTRY_CLASS : CITY_CLASS,
       },
     });
 
@@ -29,10 +34,10 @@ const apiQuery = async (country: string, city: string): Promise<GeoNames.RootObj
       params: {
         q: city,
         country,
-        maxRows: 10,
-        username: 'weknowit',
+        maxRows: ROW_COUNT,
+        username: USERNAME,
         orderBy: 'population',
-        featureClass: 'P',
+        featureClass: CITY_CLASS,
       },
     });
 
