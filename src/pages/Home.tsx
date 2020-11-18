ab2715e47ca0f1d95bd494d129dfc5a3c0da930e
Emil Blennow <emil.blennow99@gmail.com>
2020-11-18 16:23:26 +0100

    Updated style on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import Button from '../components/Button';
 import Jumbotron from '../components/Jumbotron';
 import '../styles/Home.css';
 
-const Home: React.FunctionComponent = () => (
-  <div>
-    <Jumbotron />
+const Home: React.FunctionComponent = () => {
+  const history = useHistory();
+  return (
     <div>
-      <Link to="/search-by-country">Search by country</Link>
-      <Link to="/search-by-city">Search by city</Link>
+      <Jumbotron />
+      <div className="home__button_row">
+        <Button onClick={() => history.push('/search-by-country')}>
+          Search by country
+        </Button>
+        <Button onClick={() => history.push('/search-by-city')}>
+          Search by city
+        </Button>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Home;
